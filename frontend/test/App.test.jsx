a94767284e7778c23370f5e0f840ac31e90b5f61
René Rudat <rene.rudat@wiss-edu.ch>
2026-05-16 14:50:19 +0200

    Remove unused `node:test` import from `App.test.jsx`.

--- frontend/test/App.test.jsx
+++ frontend/test/App.test.jsx
@@ -2,7 +2,6 @@ import { afterEach, beforeEach, describe, expect, jest, test } from '@jest/globa
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../src/App';
-import * as test from "node:test";
 
 describe('App', () => {
 	beforeEach(() => {
